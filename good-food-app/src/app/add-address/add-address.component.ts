aca72c90ebea48c6a4b209f47ff4ae37cde23c9a
LeRoms <romain.girard.06700@gmail.com>
2022-04-19 14:20:48 +0200

    Adds form
    Add formulaire, expiration date missing

--- good-food-app/src/app/add-address/add-address.component.ts
+++ good-food-app/src/app/add-address/add-address.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-address',
@@ -7,9 +8,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddAddressComponent implements OnInit {
 
-  constructor() { }
-
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    number: new FormControl(''),
+    adress: new FormControl(''),
+    postalCode: new FormControl(''),
+    city: new FormControl(false),
+  });
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) { }
   ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        username: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20)
+          ]
+        ],
+        number: ['',
+          [
+            Validators.required,
+            Validators.pattern("^[0-9]*$"),
+            Validators.minLength(6),
+            Validators.maxLength(20)]],
+        adress: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40)
+          ]
+        ],
+        postalCode: ['',
+          Validators.required,
+          Validators.pattern("^[0-9]*$"),
+          Validators.minLength(5), 
+          Validators.maxLength(5),
+        ],
+        city: ['', Validators.required],
+      }
+    );
   }
 
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    console.log(JSON.stringify(this.form.value, null, 2));
+  }
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
+  }
 }
--- good-food-app/src/app/add-card/add-card.component.ts
+++ good-food-app/src/app/add-card/add-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-card',
@@ -7,9 +8,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCardComponent implements OnInit {
 
-  constructor() { }
-
+  form: FormGroup = new FormGroup({
+    numCard: new FormControl(''),
+    userCard: new FormControl(''),
+    expirationDate: new FormControl(''),
+    securityCode: new FormControl(''),
+    city: new FormControl(false),
+  });
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) { }
   ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        numCard: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern("^[0-9]*$"),
+            Validators.minLength(6),
+            Validators.maxLength(20)
+          ]
+        ],
+        userCard: ['',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20)]
+        ],
+        expirationDate: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40)
+          ]
+        ],
+        securityCode: ['',
+          Validators.required,
+          Validators.pattern("^[0-9]*$"),
+          Validators.minLength(5),
+          Validators.maxLength(5),
+        ],
+      }
+    );
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    console.log(JSON.stringify(this.form.value, null, 2));
+  }
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
   }
 
 }
