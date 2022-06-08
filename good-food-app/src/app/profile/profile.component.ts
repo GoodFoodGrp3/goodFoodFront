093a8facd6d98ff5a65eb361e94e22ff33f417fb
Romain <romain.girard.06700@gmail.com>
2022-06-08 15:37:07 +0200

    Profile with Service / GET CURRENT

--- good-food-app/src/app/profile/profile.component.ts
+++ good-food-app/src/app/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Customer } from '../models/customer';
+import { CustomerService } from '../services/customer.service';
 
 @Component({
   selector: 'app-profile',
@@ -8,13 +10,37 @@ import { Router } from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  userConnected!: Customer;
+
+  constructor(private customerService: CustomerService, private router: Router) { }
 
   ngOnInit(): void {
+    console.log("LA TOKKKKENN" + sessionStorage.getItem('token'))
+    this.checkUsers(sessionStorage.getItem('token'));
   }
 
   logout() {
     sessionStorage.clear()
     this.router.navigateByUrl('/home')
+  }
+
+  checkUsers(token : any){
+    this.customerService.CheckUserToken(token).subscribe(
+      reponse => {
+        this.userConnected = reponse;
+        console.log(this.userConnected)
+        //this.getUser(reponse.id);
+      });
+  }
+
+  /**
+   Recuperation utilisateur connecté
+   */
+   getUser(id : number){
+    this.customerService.getOneUser(id).subscribe(
+      reponse => {
+        this.userConnected = reponse;
+        console.log(this.userConnected)
+      });
   }
 }
--- good-food-app/src/app/services/customer.service.ts
+++ good-food-app/src/app/services/customer.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Customer } from '../models/customer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  customer!: Customer[];
+
+  constructor(private httpClient: HttpClient, private router: Router) { }
+
+  CheckUserToken(token: any): Observable<Customer> {
+    const headers = { 'Authorization': 'Bearer ' + token}
+    return this.httpClient.get<Customer>(environment.apiUrl + '/customers/current',{'headers':headers});
+  }
+
+  /**
+   *
+   * Recuperation utilisateur par son id
+   */
+   getOneUser(id : number) : Observable<Customer> {
+    const headers = { 'Authorization': 'Bearer ' + sessionStorage.getItem('token')}
+    return this.httpClient.get<Customer>(environment.apiUrl + '/customers/' + id,{'headers':headers});
+  }
+  
 }
